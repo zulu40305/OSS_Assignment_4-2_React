359bc832d074c3f8ffc05cf3f15f21903fcc47f3
zulu40305 <zulu40305@gmail.com>
2024-11-04 17:51:27 +0900

    added React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>My_React_Page</h2>
+        <p>
+          Modified the page's CSS and HTML.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
